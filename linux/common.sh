48fe8da167e5483d484d33727def09c020d4b5a6
blademainer <blademainer@gmail.com>
2016-05-26 20:05:25 +0800

    common shell

--- linux/common.sh
+++ linux/common.sh
@@ -1,130 +1,141 @@
 #!/bin/sh
 PID=0
 DIR=`dirname $0`
-PIDFILE=$DIR/nohup.pid
-OUTFILE=$DIR/nohup.out
-PROGNAME='GoAgent'
- 
+PROGNAME=test
+PIDFILE=$DIR/$PROGNAME.pid
+OUTFILE=$DIR/$PROGNAME.out
+
+#run() {
+#    nohup $DIR/rule1.sh>>$OUTFILE 2>&1 & echo $!>$PIDFILE
+#}
 run() {
-    nohup $DIR/proxy.py>>$OUTFILE 2>&1 & echo $!>$PIDFILE
+#    java -classpath target/spark-demo-1.0-SNAPSHOT.jar \
+#    com.xunlei.demo.DeleteHdfsFile \
+#    hdfs://vipvm0061:9000/BehaviorAnalysisRuleOneToHBaseCheckPoint/
+
+    nohup \
+    `echo 'test'>test.tmp` \
+    >>$OUTFILE 2>&1 & echo $!>$PIDFILE
+
 }
- 
+
 init() {
-    if [ -f $PIDFILE ]; then
-        PID=`cat $PIDFILE`
-    fi
+  if [ -f $PIDFILE ]; then
+    PID=`cat $PIDFILE`
+  fi
 }
- 
+
 check() {
-    if [ $PID -eq 0 ]; then
-        return 1
-    else
-        kill -0 $PID 2>/dev/null
-    fi
+  if [ $PID -eq 0 ]; then
+    return 1
+  else
+    kill -0 $PID 2>/dev/null
+  fi
 }
- 
+
 start() {
-    init
-    check
-    if [ $? -eq 0 ]; then
-        echo "$PROGNAME is run!" && exit 1
-    else
-        run
-        echo "Start $PROGNAME!" && exit 0
-    fi
+  init
+  check
+  if [ $? -eq 0 ]; then
+    echo "$PROGNAME is run!" && exit 1
+  else
+    run
+    echo "Start $PROGNAME!" && exit 0
+  fi
 }
- 
+
 silent() {
-    init
-    check
-    if [ $? -eq 0 ]; then
-        exit 1
-    else
-        run
-        exit 0
-    fi
+  init
+  check
+  if [ $? -eq 0 ]; then
+    exit 1
+  else
+    run
+    exit 0
+  fi
 }
- 
+
 restart() {
-    init
-    check
-    if [ $? -eq 0 ]; then
-        kill -9 $PID>/dev/null 2>&1
-    fi
-    run
-    echo "Restart $PROGNAME!" && exit 0
+  init
+  check
+  if [ $? -eq 0 ]; then
+    kill -9 $PID>/dev/null 2>&1
+  fi
+  run
+  echo "Restart $PROGNAME!" && exit 0
 }
- 
+
 stop() {
-    init
-    check
-    if [ $? ]; then
-        kill -9 $PID>/dev/null 2>&1
-        echo "$PROGNAME is stop!" && exit 0
-    else
-        echo "$PROGNAME is not run!" && exit 1
-    fi
+  init
+  check
+  if [ $? ]; then
+    kill -9 $PID>/dev/null 2>&1
+    echo "$PROGNAME is stop!" && exit 0
+  else
+    echo "$PROGNAME is not run!" && exit 1
+  fi
 }
- 
+
 status() {
-    init
-    check
-    if [ $? -eq 0 ]; then
-        echo "$PROGNAME is run!" && exit 0
-    else
-        echo "$PROGNAME is not run!" && exit 1
-    fi
+  init
+  check
+  if [ $? -eq 0 ]; then
+    echo "$PROGNAME is run!" && exit 0
+  else
+    echo "$PROGNAME is not run!" && exit 1
+  fi
 }
- 
+
 output() {
-    if [ -f $OUTFILE ]; then
-        watch -n 1 tail $OUTFILE
-    else
-        echo "$OUTFILE is not find!" && exit 1
-    fi
+  if [ -f $OUTFILE ]; then
+    watch -n 1 tail $OUTFILE
+  else
+    echo "$OUTFILE is not find!" && exit 1
+  fi
 }
- 
+
 clean() {
-    if [ -f $OUTFILE ]; then
-        rm -f $OUTFILE
-    fi
-    if [ -f $PIDFILE ]; then
-        rm -f $PIDFILE
-    fi
-    echo 'Clean success!' && exit 0
+  if [ -f $OUTFILE ]; then
+    rm -f $OUTFILE
+  fi
+  if [ -f $PIDFILE ]; then
+    rm -f $PIDFILE
+  fi
+  echo 'Clean success!' && exit 0
 }
- 
+
 case "$1" in
-    start)
-        $1
-        exit 0
-        ;;
-    silent)
-        $1
-        exit 0
-        ;;
-    restart)
-        $1
-        exit 0
-        ;;
-    stop)
-        $1
-        exit 0
-        ;;
-    status)
-        $1
-        exit 0
-        ;;
-    output)
-        $1
-        exit 0
-        ;;
-    clean)
-        $1
-        exit 0
-        ;;
-    *)
-        echo $DIR
-        echo 'Usage: {start|stop|restart|silent|status|output|clean}'
-        ;;
+  start)
+    $1
+    exit 0
+    ;;
+  silent)
+    $1
+    exit 0
+    ;;
+  restart)
+    $1
+    exit 0
+    ;;
+  stop)
+    $1
+    exit 0
+    ;;
+  status)
+    $1
+    exit 0
+    ;;
+  output)
+    $1
+    exit 0
+    ;;
+  clean)
+    $1
+    exit 0
+    ;;
+  *)
+    echo $DIR
+    echo 'Usage: {start|stop|restart|silent|status|output|clean}'
+    ;;
 esac
+
